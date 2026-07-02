2aa24ae39f1f29c0702f8b6fe34ffb3f97d50b17
HaroTorrado73 <ubuntohcmelot@gmail.com>
2026-07-01 19:54:07 -0500

    refactor(services): export phase 3 service modules

--- rf_sentinel/services/__init__.py
+++ rf_sentinel/services/__init__.py
@@ -2,6 +2,7 @@
 
 from .capture_service import CaptureService
 from .export_service import ExportService
+from .pipeline import DetectionPipeline
 from .scan_service import ScanService
 
-__all__ = ["ScanService", "CaptureService", "ExportService"]
+__all__ = ["ScanService", "CaptureService", "ExportService", "DetectionPipeline"]

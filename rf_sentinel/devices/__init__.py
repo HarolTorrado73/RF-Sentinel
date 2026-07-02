05d1bfcd85d19fbecf4c61beb74adf8f789dcbf9
HaroTorrado73 <ubuntohcmelot@gmail.com>
2026-07-01 19:54:12 -0500

    refactor(devices): update package exports

--- rf_sentinel/devices/__init__.py
+++ rf_sentinel/devices/__init__.py
@@ -2,6 +2,8 @@
 
 from .base import SDRDevice
 from .hackrf import HackRFSource
+from .mock import MockSDR
+from .registry import DeviceRegistry
 from .rtl_sdr import RTLSDRSource
 
-__all__ = ["SDRDevice", "RTLSDRSource", "HackRFSource"]
+__all__ = ["SDRDevice", "RTLSDRSource", "HackRFSource", "MockSDR", "DeviceRegistry"]
